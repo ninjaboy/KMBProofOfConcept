fd2c1300b6293acd164e2fab1a9c2d28ff4fa651
Alessio Peternelli <alessio.peternelli@gmail.com>
2018-05-15 16:22:31 +0100

    First commit!

---
+++ KMBProofOfConcept/Controllers/ValuesController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KMBProofOfConcept.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        [HttpPost]
+        public string Post([FromBody]string value)
+        {
+            return value;
+        }
+    }
+}
---
+++ KMBProofOfConcept/RandomMiddleware.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace KMBProofOfConcept
+{
+    public class RandomMiddleware
+    {
+        private readonly RequestDelegate next;
+
+        public RandomMiddleware(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            await next.Invoke(context);
+        }
+    }
+}
